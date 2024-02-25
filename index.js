4000a4105a9638189fc20921dd4edfac02fcd9d3
yhassanige1 <yanny.hassani@universite-paris-saclay.fr>
2024-02-25 01:37:36 +0100

    toujours

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const date = new Date();
 const day = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
 
 const generateNewREADME = (path) =>{
-    const state = date.getDay() == 6 || date.getDay() == 7 ? '🌴 repos.' : '🏢 travail.';
+    const state = date.getDay() == 0 || date.getDay() == 7 ? '🌴 repos.' : '🏢 travail.';
+    console.log(state)
     const final = `<h1 align='center'>Hi 👋, I'm yhassanige1</h1>\n<div align='center'>\n\n|<h2 align='center'>Nous sommes le: <u>${date.toLocaleDateString('en-GB')}</u></h2><h2 align='center'>c'est un ${day[date.getDay() - 1]} et je suis au: ${state}</h2>|\n|---\n</div>`;
     fs.writeFile(path, final, (err) =>{
         if(err)
@@ -12,5 +13,7 @@ const generateNewREADME = (path) =>{
         }
     });
 };
+
+
 
 generateNewREADME('README.md');
